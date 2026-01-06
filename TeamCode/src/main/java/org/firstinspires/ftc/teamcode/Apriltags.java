0a2e9db100171f51b31040ae65c5dfc246452687
dpinzon27 <dnnapnzn@gmail.com>
2026-01-06 16:23:56 -0600

    FOR FAE!!! (Motor functions done!):D flywheel done
    just some localization stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Apriltags.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Apriltags.java
@@ -10,9 +10,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-//changes
-@TeleOp
+
 @Autonomous(name = "Apriltagstest", group = "OpMode")
 public class Apriltags extends OpMode {
 
