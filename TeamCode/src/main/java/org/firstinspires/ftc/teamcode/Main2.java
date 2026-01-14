087bd195552964f8108f6134c7c0d763b37fb45b
dpinzon27 <dnnapnzn@gmail.com>
2026-01-13 18:29:37 -0600

    FOR FAE!!! (Motor functions done!):D flywheel done
    just some localization stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main2.java
@@ -106,7 +106,7 @@ public class Main2 extends LinearOpMode {
                 max = Math.max(max, Math.abs(leftBackPower));
                 max = Math.max(max, Math.abs(rightBackPower));
 
-                if (max > 0.8) {
+                if (max > 0.85) {
                     leftFrontPower /= max;
                     rightFrontPower /= max;
                     leftBackPower /= max;
