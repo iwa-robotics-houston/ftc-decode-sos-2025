107f7290d8058c1165be285611913725e1d0148b
dpinzon27 <dnnapnzn@gmail.com>
2026-02-03 19:20:22 -0600

    FOR FAE!!! (Motor functions done!):D flywheel done
    just some localization stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fromAfarBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fromAfarBlue.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@Autonomous(name = "BackFromBlue", group = "OpMode")
+@Autonomous(name = "fromAfarBlue", group = "OpMode")
 public class fromAfarBlue extends LinearOpMode {
 
     private Robot robot;
