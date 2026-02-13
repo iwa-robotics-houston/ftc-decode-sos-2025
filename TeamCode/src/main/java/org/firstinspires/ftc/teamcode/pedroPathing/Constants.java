91f7c5e8fdcb05f5734f23f3e3cbb567aece3aca
dpinzon27 <dnnapnzn@gmail.com>
2026-02-12 18:29:29 -0600

    FOR FAE!!! (Motor functions done!):D flywheel
    PEDRO PASCALING

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -78,8 +78,8 @@ public class Constants {
 
     public static PathConstraints pathConstraints = new PathConstraints(0.99,
             100,
-            2.0,
-            2.0);
+            1.0,
+            1.0);
 
 
 
