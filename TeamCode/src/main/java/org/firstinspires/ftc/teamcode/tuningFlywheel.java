f56df0b4ab41f101006ed4e5bbd73fb79ac3493a
dpinzon27 <dnnapnzn@gmail.com>
2026-04-06 15:18:09 -0500

    FOR FAE!!! (Motor functions done!):D flywheel
    PEDRO PASCALING

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuningFlywheel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuningFlywheel.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
@@ -53,7 +54,56 @@ public class tuningFlywheel extends OpMode {
 
     @Override
     public void loop() {
-        //get our gamepad commands
+        if (gamepad1.yWasPressed()) {
+            if (cuTargetVelcoity2 == highVelocity) {
+                cuTargetVelcoity2 = lowVelocity;
+            } else {
+                cuTargetVelcoity2 = highVelocity;
+            }
+        }
+        if (gamepad1.bWasPressed()) {
+            stepIndex = (stepIndex + 1) % stepSizes.length;
+        }
+        if (gamepad1.dpadLeftWasPressed()) {
+            F -= stepSizes[stepIndex];
+        }
+        if (gamepad1.dpadRightWasPressed()) {
+            F += stepSizes[stepIndex];
+        }
+        if (gamepad1.dpadUpWasPressed()) {
+            P += stepSizes[stepIndex];
+        }
+        if (gamepad1.dpadDownWasPressed()) {
+            P -= stepSizes[stepIndex];
+        }
+
+        // update PIDF and run both motors
+        PIDFCoefficients pidfCoefficients = new PIDFCoefficients(P, 0, 0, F);
+        flywheel1.setPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, pidfCoefficients);
+        flywheel2.setPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, pidfCoefficients);
+
+        double velocity1 = flywheel1.getVelocity();
+        double velocity2 = flywheel2.getVelocity();
+        double TargetVelocity = (velocity1 + velocity2) / 2.0;
+        double error = cuTargetVelcoity2 - TargetVelocity;
+        // rest of your telemetry code...
+
+        telemetry.addData("Target Velocity", cuTargetVelcoity2);
+        telemetry.addData("Current Velocity, %.2f", TargetVelocity);
+        telemetry.addData("Error", "%.2f", error);
+        telemetry.addLine("---------------------");
+        telemetry.addData("Tuning P", "%.4f (D-Pad U/D)", P);
+        telemetry.addData("Tuning F", "%.4f (D-Pad L/R)", F);
+        telemetry.addData("Step Size", "%.4f (B Button)", stepSizes[stepIndex]);
+    }
+}
+
+
+
+
+
+
+        /*//get our gamepad commands
         //set target vleocity
         //update etelemetry
         if (gamepad1.yWasPressed()){
@@ -109,4 +159,4 @@ public class tuningFlywheel extends OpMode {
 
 
     }
-}
+}*/
