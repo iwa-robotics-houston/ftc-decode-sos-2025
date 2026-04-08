80584b327d1ed598e1c51469aeb5e4b57d522d6b
dpinzon27 <dnnapnzn@gmail.com>
2026-04-08 16:22:38 -0500

    FOR FAE!!! (Motor functions done!):D flywheel
    PEDRO PASCALING

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main2.java
@@ -172,7 +172,7 @@ public class Main2 extends LinearOpMode {
 
             // SHOOTER MODE SELECT
             if (gamepad2.x) {
-                targetVelocity = 1400;  ///1315
+                targetVelocity = 1500;  ///1315
                 readyColor = RevBlinkinLedDriver.BlinkinPattern.BLUE_GREEN; // purple
             } else if (gamepad2.y) {
                 targetVelocity = 1505;
