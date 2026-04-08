1992a8aa6d52e57f6c148d5e484cb38e64c4b1b6
dpinzon27 <dnnapnzn@gmail.com>
2026-04-08 15:20:37 -0500

    FOR FAE!!! (Motor functions done!):D flywheel
    PEDRO PASCALING

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main2.java
@@ -121,19 +121,34 @@ public class Main2 extends LinearOpMode {
             robot.rightBackDrive.setPower(rightBackPower);
 
 
-            double leg1 = 1;
+            /*double leg1 = 1;
             double leg2 = 1;
             if (gamepad2.dpad_down) { //was: dpad_up edit: reverted
                 robot.lift1.setPower(-leg1);
                 robot.lift2.setPower(-leg2);
                 //robot.arm.setTargetPosition(robot.arm.getCurrentPosition()-10);
-            } else if (gamepad2.dpad_up) { //was: dpad_down edit: reverted
+            } else if (gamepad2.dpad_Up) { //was: d pad_down edit: reverted
                 robot.lift1.setPower(leg1);
                 robot.lift2.setPower(leg2);
             } else {
                 robot.lift1.setPower(0);
                 robot.lift2.setPower(0);
+            }*/
+
+
+            if (gamepad2.y) {
+                robot.lift1.setPower(1);
+                robot.lift2.setPower(1);
             }
+            else if (gamepad2.x) {
+                robot.lift1.setPower(-1);
+                robot.lift2.setPower(-1);
+            } else {
+                robot.lift1.setPower(0.0);
+                robot.lift2.setPower(0.0);
+            }
+
+
             // INTAKE
             if (gamepad2.left_trigger > 0) {
                 robot.rollerIntake.setPower(1);
